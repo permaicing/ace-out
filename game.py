bc1199ea6588e6852373034f100c2d66a959d890
syamego <jl.5892@pm.me>
2024-03-25 22:08:46 +0000

    Added basic functionality

--- game.py
+++ game.py
@@ -1,6 +1,30 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from objects import Ship
+
 
 class Game:
 
-    @staticmethod
-    def run():
-        pass
+    def __init__(self):
+        self.ship = Ship([0, -0.8], [1, 0, 0], 0.1, 0.01)
+
+    # Handlers
+    def keyboard(self, key, x, y):
+        if key == b'\x1B':
+            glutLeaveMainLoop()
+        elif key == b'a':
+            self.ship.move_left(self.ship.speed)
+        elif key == b'w':
+            self.ship.move_up(self.ship.speed)
+        elif key == b's':
+            self.ship.move_down(self.ship.speed)
+        elif key == b'd':
+            self.ship.move_right(self.ship.speed)
+
+    def run(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+        glLoadIdentity()
+
+        self.ship.draw()
+
+        glutSwapBuffers()
--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from OpenGL.GLUT import *
 from game import Game
 
 
-
-
 if __name__ == '__main__':
     glutInit()
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
-    glutInitWindowSize(320, 640)
+    glutInitWindowSize(640, 640)
     glutCreateWindow('Ace-Out')
-    glutDisplayFunc(Game.run)
     glClearColor(0, 0, 0, 1)
+    
+    game = Game()
+
+    glutKeyboardFunc(game.keyboard)
+    glutDisplayFunc(game.run)
+    glutIdleFunc(game.run)
+    
     glutMainLoop()
