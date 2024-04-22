52d64f06a4b8ca89386fa22670559ceef56352c0
syamego <jl.5892@pm.me>
2024-04-23 00:18:03 +0100

    Fixed 's visual indirection

--- src/objects/DirectedProjectile.py
+++ src/objects/DirectedProjectile.py
@@ -1,5 +1,6 @@
+from math import acos, degrees
 from src.primitives.Triangle import Triangle
-from glm import vec3, normalize
+from glm import vec3, vec4, normalize
 
 class DirectedProjectile(Triangle):
     def __init__(self, x, y, target):
@@ -8,7 +9,9 @@ class DirectedProjectile(Triangle):
             vec3(0.5, -0.5, 1), # Scale
             (0, 1, 0) # Color
         )
-        self.velocity = 0.25*normalize(target - self.position)
+        aux = normalize(target - self.position)
+        self.velocity = 0.25*aux
+        self.rotation = vec4(0, 0, 1, -90+degrees(acos(aux.x)))
         self.isEnemy = True
     
     def updatePosition(self):
