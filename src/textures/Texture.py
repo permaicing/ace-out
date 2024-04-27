5054c1570ffde3ae30d6112d927fcbcf81c0ff41
kaelsiebra <kaelsiebra233@gmail.com>
2024-04-27 14:56:37 -0300

    added the texture handling class

---
+++ src/textures/Texture.py
@@ -0,0 +1,22 @@
+from OpenGL.GL import *
+from PIL import Image
+
+class Texture():
+    def __init__(self, filepath):
+        self.texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+        glTexParameter(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameter(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        image = Image.open(filepath).transpose(Image.FLIP_TOP_BOTTOM)
+        imageData = image.convert("RGBA").tobytes()
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData)
+        glGenerateMipmap(GL_TEXTURE_2D)
+
+    def useTexture(self):
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+
+    def destroyTexture(self):
+        glDeleteTextures(1, self.texture)
